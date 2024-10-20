66e1e1695a27f34aefcd2b2506f0fefd073b6a29
Igor <malysh_ii@nlmk.com>
2024-10-20 20:51:42 +0300

    feature: added grafana monitoring

--- backend/settings.py
+++ backend/settings.py
@@ -5,6 +5,8 @@ from flask_cors import CORS, cross_origin
 from flask_openapi3 import Info, Tag
 from flask_openapi3 import OpenAPI
 
+from utils import setting_statsd, StatsdMiddleware
+
 
 UPLOAD_FOLDER = './files'
 
@@ -13,6 +15,9 @@ app = OpenAPI(__name__, info=info)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:admin@postgres-backend:5432/project'
+
+setting_statsd()
+app.wsgi_app = StatsdMiddleware(app.wsgi_app, "flask-monitoring")
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
